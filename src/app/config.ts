e5513234bfd5bc10587d42481743b48ab839e66a
KishiTheMechanic <s.kishi@elsoul.nl>
2024-08-25 02:30:28 +0200

    delete iconUrl

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,3 @@
-export const logoUrl =
-  'https://pub-f5c1b877dd3a4b63aacd87ee9a480be9.r2.dev/SkeetLogoHorizontal.png'
-
 export const locales = ['en', 'ja'] as const
 export const defaultLocale = 'en'
 
