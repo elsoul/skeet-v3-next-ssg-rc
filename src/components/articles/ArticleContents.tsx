b0f6e351fd3dcec8174e952e0140871f4824b06c
KishiTheMechanic <s.kishi@elsoul.nl>
2024-08-26 07:58:04 +0200

    improve typography

--- src/components/articles/ArticleContents.tsx
+++ src/components/articles/ArticleContents.tsx
@@ -20,51 +20,33 @@ export default function ArticleContents({ content }: Props) {
         components={{
           h1({ children, ...props }) {
             return (
-              <h1
-                className="mb-2 mt-12 text-3xl font-bold tracking-tight"
-                id={props.id}
-              >
+              <h1 className="mt-6 tracking-tight" id={props.id}>
                 {children as React.ReactNode}
               </h1>
             )
           },
           h2({ children, ...props }) {
             return (
-              <h2
-                className="mb-2 mt-12 text-2xl font-bold tracking-tight"
-                id={props.id}
-              >
+              <h2 className="tracking-tight" id={props.id}>
                 {children as React.ReactNode}
               </h2>
             )
           },
           h3({ children, ...props }) {
             return (
-              <h3
-                className="mb-1 mt-8 text-xl font-bold tracking-tight"
-                id={props.id}
-              >
+              <h3 className="tracking-tight" id={props.id}>
                 {children as React.ReactNode}
               </h3>
             )
           },
           h4({ children, ...props }) {
             return (
-              <h4
-                className="mb-1 mt-6 text-lg font-bold tracking-tight"
-                id={props.id}
-              >
+              <h4 className="tracking-tight" id={props.id}>
                 {children as React.ReactNode}
               </h4>
             )
           },
-          p({ children, ...props }) {
-            return (
-              <p className="mb-4 mt-3 last:mb-4" id={props.id}>
-                {children as React.ReactNode}
-              </p>
-            )
-          },
+
           img({ children, ...props }) {
             return (
               <>
@@ -95,7 +77,7 @@ export default function ArticleContents({ content }: Props) {
 
             return (
               <a
-                className="underline hover:opacity-80"
+                className="underline hover:opacity-70"
                 id={props.id}
                 href={href}
                 target="_blank"
@@ -105,27 +87,6 @@ export default function ArticleContents({ content }: Props) {
               </a>
             )
           },
-          ol({ children, ...props }) {
-            return (
-              <ol className="mb-4 ml-8 mt-3 block list-decimal" id={props.id}>
-                {children as React.ReactNode}
-              </ol>
-            )
-          },
-          ul({ children, ...props }) {
-            return (
-              <ul className="mb-4 ml-8 mt-3 list-disc" id={props.id}>
-                {children as React.ReactNode}
-              </ul>
-            )
-          },
-          li({ children, ...props }) {
-            return (
-              <li className="py-1.5" id={props.id}>
-                {children as React.ReactNode}
-              </li>
-            )
-          },
 
           code({ node, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || '')
@@ -133,13 +94,7 @@ export default function ArticleContents({ content }: Props) {
 
             if (!match) {
               return (
-                <code
-                  className={cn(
-                    className,
-                    'rounded-md border border-zinc-400 bg-zinc-200 p-1 text-red-500',
-                  )}
-                  {...props}
-                >
+                <code className={cn(className)} {...props}>
                   {/* @ts-ignore */}
                   {children}
                 </code>
